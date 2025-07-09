f1919ee96df561e6546b3263e3501b0d5cdd8c80
Wilfred007 <adzerwilfred007@gmail.com>
2025-07-09 22:58:52 +0100

    latest commit

--- submissions/week-1/day-2/Wilfred_Adzer_Basic_Arithmetic_Op_In_Rust/target/debug/deps/Wilfred_Adzer_Basic_Arithmetic_Op_In_Rust-b59dad08214fde22.d
+++
@@ -1,5 +0,0 @@
-/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-2/Wilfred_Adzer_Basic_Arithmetic_Op_In_Rust/target/debug/deps/Wilfred_Adzer_Basic_Arithmetic_Op_In_Rust-b59dad08214fde22: src/main.rs
-
-/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-2/Wilfred_Adzer_Basic_Arithmetic_Op_In_Rust/target/debug/deps/Wilfred_Adzer_Basic_Arithmetic_Op_In_Rust-b59dad08214fde22.d: src/main.rs
-
-src/main.rs:
