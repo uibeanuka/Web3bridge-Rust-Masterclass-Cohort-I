7f8b158489de0ca09d43f4a0f5caa15d1f48ea65
Derrick <uibeanuka@gmail.com>
2025-07-15 12:55:24 +0100

    third commit

--- Derrick-class-task/task/src/main.rs
+++ Derrick-class-task/task/src/main.rs
@@ -1,2 +1,14 @@
-pub mod smart;
-use crate::smart::{ClassManagement, Student, StudentStatus};
+mod smart;
+use smart::{ClassManagement, Student, StudentStatus};
+
+fn main() {
+    let mut class = ClassManagement::new();
+
+    class.register_student("Alice".to_string(), "A".to_string());
+    class.register_student("Bob".to_string(), "B".to_string());
+
+    let students = class.get_all_students();
+    for student in students {
+        println!("Name: {}, Grade: {:?}", student.name, student.grade);
+    }
+}
